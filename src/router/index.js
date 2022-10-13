ace490a8b7d5d3dace0a03ca7b9fda3932cbe267
clement <c.gambier@gustosolutions.fr>
2022-10-13 17:10:07 +0200

    link router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Edit from '@/views/Edit.vue'
+import Bring from '@/views/Bring.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/edit/:id',
     name: 'Edit',
     component: Edit
+  },
+  {
+      path: '/bring',
+      name: 'Bring',
+      component: Bring
   }
 ]
 
