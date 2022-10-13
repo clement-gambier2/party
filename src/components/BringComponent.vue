11811bf2c6627b143206a66797b6daae7d6685d6
clement <c.gambier@gustosolutions.fr>
2022-10-13 17:10:27 +0200

    bring

---
+++ src/components/BringComponent.vue
@@ -0,0 +1,3 @@
+<template>
+  <p> oui </p>
+</template>
---
+++ src/views/Bring.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h1> Ce que j'apporte </h1>
+    <BringComponent />
+  </section>
+</template>
+
+
+<script>
+  import BringComponent from "@/components/BringComponent.vue";
+  export default {
+    components: { BringComponent }
+  }
+
+
+</script>
