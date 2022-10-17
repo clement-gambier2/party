2c88e16d434544e8ce22df6d6804f2f42ec63938
clement <c.gambier@gustosolutions.fr>
2022-10-17 14:03:22 +0200

    hero banner

--- src/components/Birthday.vue
+++ src/components/Birthday.vue
@@ -1,27 +1,57 @@
 <template>
-  <header>
-    <h1>Les 25 ans d'Hélène 🎉😙</h1>
-    <!-- <img src="../assets/guitar.jpeg"> -->
+  <header >
+    <div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
+      <div class="flex flex-wrap items-center sm:-mx-3">
+        <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
+          <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+            <span class="block xl:inline">Grosse Teuf</span>
+          <span class="block text-transparent text-7xl bg-clip-text bg-gradient-to-r from-[#9FBACD] to-[#F2C144] xl:inline"> d'anniversaire</span>
+          </h1>
+          <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">Oh la chance tu es invité.e à ma super teuf de pré-anniversaire ! </p>
+        </div>
+        <img src="../assets/hln.jpg" class="max-w-full h-auto rounded-lg">
+      </div>
+    </div>
   </header>
-  <section>
-  <h2>Quand ? Comment ? Où ?  🧐</h2>
-    <ul>
-      <li>Le Samedi 10 Décembre à 18h30 ! </li>
-      <li> Add to calendar</li>
-      <li>À la panoramique, notre super appartement à Clément et moi. Autrement dit au 10 avenue de l'Architecte Cordonnier à Lille.</li>
-      <li>Google Map integration</li>
-    </ul>
+
+  <section class="container items-center max-w-6xl px-4 px-10 mx-auto sm:px-20 md:px-32 lg:px-16 my-20">
+    <div class="flex flex-wrap items-center -mx-3">
+      <div class="order-1 w-full px-3 lg:w-1/2 lg:order-0">
+        <div class="w-full lg:max-w-md">
+          <h2 class="mb-4 text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading">
+            <span class="text-[#F15BB5]"> Quand ? </span>
+            <span class="text-[#00F5D4]"> Où ? </span>
+            <span class="text-[#00BBF9]"> Comment ? </span>
+          </h2>
+          <ul>
+            <li class="flex items-center py-2 space-x-4 xl:py-3">
+              <p>🎉</p>
+            <span class="font-medium text-gray-500">Samedi 10 Décembre. <br> Heure officielle : 20h <br> Heure officieuse : 17h15 l'heure du ricard !</span>
+            </li>
+            <li class="flex items-center py-2 space-x-4 xl:py-3">
+              <p>🎂</p>
+              <span class="font-medium text-gray-500">Clément et moi vous accueillons à <span>✨La Panoramique✨</span><br>10 avenue de l'Architecte Cordonnier à Lille. <br> Étage 11, Appartement 43.</span>
+            </li>
+            <li class="flex items-center py-2 space-x-4 xl:py-3">
+              <p>🔥</p>
+              <span class="font-medium text-gray-500">Thème : En bombe bb.</span>
+            </li>
+          </ul>
+
+    </div>
+    </div>
+    <div class="w-full px-3 mb-12 lg:w-1/2 order-0 lg:order-1 lg:mb-0"><img class="mx-auto sm:max-w-sm lg:max-w-full" src="https://cdn.devdojo.com/images/november2020/feature-graphic.png" alt="feature image"></div>
+    </div>
   </section>
+
 </template>
 <script>
-export default {
-  
-}
+export default {}
 </script>
 
 <style>
 img {
-  width: 20%;
+  width: 50%;
   height: auto;
 }
 </style>
