a537a3736545e669f4775d7587dfc79a3650a97d
clement <c.gambier@gustosolutions.fr>
2022-10-21 23:14:52 +0200

    details

--- src/components/Birthday.vue
+++ src/components/Birthday.vue
@@ -1,37 +1,44 @@
 <template>
   <header >
     <div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
-      <div class="flex flex-wrap items-center sm:-mx-3">
+      <div>
         <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
           <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
             <span class="block xl:inline">Grosse Teuf</span>
-          <span class="block text-transparent text-5xl bg-clip-text bg-gradient-to-r from-[#9FBACD] to-[#F2C144] xl:inline"> d'anniversaire</span>
+            <span class="block text-transparent text-5xl bg-clip-text bg-gradient-to-r from-[#9FBACD] to-[#F2C144] xl:inline"> d'anniversaire</span>
           </h1>
           <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">Oh la chance tu es invité.e à ma super teuf de pré-anniversaire ! </p>
         </div>
-        <img src="../assets/hln.jpg" class="max-w-full h-auto rounded-lg">
+        
       </div>
     </div>
+    <div class="flex justify-center w-1200">
+      <img src="../assets/hln.jpg" class="max-w-full h-auto rounded-lg">
+    </div>
+    
   </header>
 
   <section class="container items-center max-w-6xl px-4 px-10 mx-auto sm:px-20 md:px-32 lg:px-16 my-20">
     <div class="flex flex-wrap items-center -mx-3">
       <div class="order-1 w-full px-3 lg:w-1/2 lg:order-0">
         <div class="w-full lg:max-w-md">
-          <h2 class="mb-4 text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading">
-            <span class="text-[#F15BB5]"> Quand ? </span>
-            <span class="text-[#00F5D4]"> Où ? </span>
-            <span class="text-[#00BBF9]"> Comment ? </span>
-          </h2>
+          <div class="flex ">
+            
+            
+            
+          </div>
           <ul>
+            <p class="text-[#95ADB9] text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading">Quand ?</p>
             <li class="flex items-center py-2 space-x-4 xl:py-3">
               <p>🎉</p>
-            <span class="font-medium text-gray-500">Samedi 10 Décembre. <br> Heure officielle : 20h <br> Heure officieuse : 17h15 l'heure du ricard !</span>
+              <span class="font-medium text-gray-500">Samedi 10 Décembre. <br> Heure officielle : 20h <br> Heure officieuse : 17h15 l'heure du ricard !</span>
             </li>
+            <p class="text-[#C1B179] text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading"> Où ? </p>
             <li class="flex items-center py-2 space-x-4 xl:py-3">
               <p>🎂</p>
               <span class="font-medium text-gray-500">Clément et moi vous accueillons à <span>✨La Panoramique✨</span><br>10 avenue de l'Architecte Cordonnier à Lille. <br> Étage 11, Appartement 43.</span>
             </li>
+            <p class="text-[#E4B446] text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading"> Comment ? </p>
             <li class="flex items-center py-2 space-x-4 xl:py-3">
               <p>🔥</p>
               <span class="font-medium text-gray-500">Thème : En bombe bb.</span>
@@ -40,7 +47,6 @@
 
     </div>
     </div>
-    <div class="w-full px-3 mb-12 lg:w-1/2 order-0 lg:order-1 lg:mb-0"><img class="mx-auto sm:max-w-sm lg:max-w-full" src="https://cdn.devdojo.com/images/november2020/feature-graphic.png" alt="feature image"></div>
     </div>
   </section>
 
