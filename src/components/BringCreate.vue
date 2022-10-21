a4e4098e7dac601d8ff5dd555ac9c64918308416
clement <c.gambier@gustosolutions.fr>
2022-10-21 22:07:35 +0200

    background color

--- src/components/BringCreate.vue
+++ src/components/BringCreate.vue
@@ -1,14 +1,13 @@
 <template>
-  <section class="flex justify-center">
-    <div class="block px-20 py-6 max-w-sm bg-red-500 rounded-lg border border-gray-200 shadow-md mb-10">
-      <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Ce que j'apporte</h2>
+  <section class="flex justify-center mt-10">
+    <div class="block px-20 py-6 max-w-sm bg-gradient-to-r from-[#9FBACD] to-[#F2C144] rounded-lg border border-gray-200 shadow-md mb-10">
+      <h2 class="mb-2 text-2xl font-bold tracking-tight text-slate-100">Tu veux apporter quelque chose ?</h2>
       <form @submit.prevent="onSubmit">
-        <div class="form-group">
-          <h2>Met ce que t'ammènes</h2>
-          <input v-model="form.name" class="form-control" required placeholder="objet"/>
-          <input v-model="form.object" class="form-control" required placeholder="Villageoise"/>
+        <div class="flex flex-col justify-center">
+          <input v-model="form.name" class="my-5" required placeholder="Ton petit nom"/>
+          <input v-model="form.object" class="mb-5" required placeholder="Objet"/>
+          <button type="submit" class="inline-block rounded-md bg-green-500 px-6 py-2 font-semibold text-green-100 shadow-md duration-75 hover:bg-green-400">Ajouter</button>
         </div>
-        <button type="submit" class="inline-block rounded-md bg-green-500 px-6 py-2 font-semibold text-green-100 shadow-md duration-75 hover:bg-green-400">Ajouter</button>
       </form>
     </div>
   </section>
--- src/components/UserCreate.vue
+++ src/components/UserCreate.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="block px-20 py-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md mb-10">
-    <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Alors chaud 😗 ?</h2>
+  <div class="block px-20 py-6 max-w-sm bg-gradient-to-r from-[#9FBACD] to-[#F2C144] rounded-lg border border-gray-200 shadow-md mb-10">
+    <h2 class="mb-2 text-2xl font-bold tracking-tight text-slate-100">Alors chaud 😗 ?</h2>
     <form @submit.prevent="onSubmit">
       <div class="form-group">
         <input v-model="form.name" class="form-control" required placeholder="Rentres ton nom" />
       </div>
       <div class="form-group mt-3">
-        <label class="font-normal text-gray-700 dark:text-gray-400">Disponible</label>
+        <label class="font-normal text-slate-100">Disponible</label>
         <div class="flex">
           <input type="checkbox" id="checkbox" v-model="form.available">
-          <p class="ml-2">Let's go 🍾</p>
+          <p class="ml-2 text-slate-100">Let's go 🍾</p>
         </div>
         <div class="flex">
           <input type="checkbox" v-model="form.notAvailable">
-          <p class="ml-2">Non 🥴</p>
+          <p class="ml-2 text-slate-100">Non 🥴</p>
         </div>
       </div>
 
